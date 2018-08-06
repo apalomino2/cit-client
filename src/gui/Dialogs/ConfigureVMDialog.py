454725cb2841e81a59006ccc323c10254a5a43cb
root <raistlinJ@users.noreply.github.com>
2018-08-05 22:04:26 -0600

    Retrieve and choose VM through GUI

--- src/gui/Dialogs/ConfigureVMDialog.py
+++ src/gui/Dialogs/ConfigureVMDialog.py
@@ -2,6 +2,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gui.Widgets.VMTreeWidget import VMTreeWidget
+from gui.Dialogs.VMRetrieveDialog import VMRetrieveDialog
+import logging
 
 class ConfigureVMDialog(Gtk.Dialog):
     def __init__(self, parent):
@@ -10,15 +12,14 @@ class ConfigureVMDialog(Gtk.Dialog):
              Gtk.STOCK_OK, Gtk.ResponseType.OK))
              
         self.vmName = ""
-        self.set_default_size(350, 100)
+        self.set_default_size(500, 300)
 
         label = Gtk.Label("Select a VM and Adaptor")
 
         # Here we will place the tree view
-        treeWidget = VMTreeWidget()
+        treeWidget = VMTreeWidget(self)
         self.connect("response", self.dialogResponseActionEvent)
-
-        treeWidget.populateTreeStore(['a', 'b'])
+            
         select = treeWidget.treeView.get_selection()
         select.connect("changed", self.onItemSelected)
         
@@ -29,18 +30,29 @@ class ConfigureVMDialog(Gtk.Dialog):
         box.add(treeWidget)
         
         self.show_all()
+    
+        vmRetrieveDialog = VMRetrieveDialog(self)
+        vmRetrieveDialog.run()
+        s = vmRetrieveDialog.getFinalData()
+        vms = s["mgrStatus"]["vmstatus"]
+        vmRetrieveDialog.destroy()
+        
+        treeWidget.populateTreeStore(vms)
         
     def onItemSelected(self, selection):
         model, treeiter = selection.get_selected()
-
         if treeiter == None:
             return
-
         self.vmName = model[treeiter][0]
+        self.adaptorSelected = model[treeiter][1]
+        if "\"none\"" in self.adaptorSelected or "adaptor disabled" in self.adaptorSelected:
+            self.get_widget_for_response(Gtk.ResponseType.OK).set_sensitive(False)
+            return
+            
+        self.get_widget_for_response(Gtk.ResponseType.OK).set_sensitive(True)
             
     def dialogResponseActionEvent(self, dialog, responseID):
         # OK was clicked and there is text
         if responseID == Gtk.ResponseType.OK:
-            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.WARNING, Gtk.ButtonsType.OK, "OK Button pressed, " + self.vmName + " selected")
-            dialog.run()
-            dialog.destroy()
+            logging.debug("dialogResponseActionEvent(): OK was pressed: " + self.vmName + " " + self.adaptorAdaptorSelected)
+        
--- src/gui/Widgets/VMTreeWidget.py
+++ src/gui/Widgets/VMTreeWidget.py
@@ -5,7 +5,9 @@ from engine.Engine import Engine
 
 class VMTreeWidget(Gtk.Grid):
 
-    def __init__(self):
+    def __init__(self, parent):
+        self.parent = parent
+        self.vmList = []
         logging.debug("Creating VMTreeWidget")
         super(VMTreeWidget, self).__init__()
 
@@ -13,23 +15,28 @@ class VMTreeWidget(Gtk.Grid):
         self.set_column_homogeneous(True)
         self.set_row_homogeneous(True)
         
-        self.liststore_manufacturers = Gtk.ListStore(str)
-        self.adaptors = ["Sony", "LG",
-            "Panasonic", "Toshiba", "Nokia", "Samsung"]
-        #for item in self.manufacturers:
-        #    self.liststore_manufacturers.append([item])
+        self.liststore_manufacturers = Gtk.ListStore(str)        
 
         # Initialized fields
-        self.treeStore = Gtk.TreeStore(str)
+        self.treeStore = Gtk.TreeStore(str, str)
         self.treeView = Gtk.TreeView(self.treeStore)
         self.scrollableTreeList = Gtk.ScrolledWindow()
         
         self.drawTreeView()
         self.setLayout()
 
-    def populateTreeStore(self, registeredVMList):
-        for vm in registeredVMList:
-            treeIter = self.treeStore.append(None, [vm])
+    def populateTreeStore(self, vmList):
+        self.vmList = vmList
+        for vm in self.vmList:
+            logging.debug("populateTreeStore(): working with: " + str(vm))
+            adaptor = str("1 - " + self.vmList[vm]["adaptorInfo"]["1"])
+            if "\"none\"" in adaptor:
+                adaptor = "1 - *adaptor disabled*"
+            treeIter = self.treeStore.append(None, [vm, adaptor])
+        self.adaptors = ["1", "2",
+            "3", "4", "5", "6", "7", "8"]
+        for item in self.adaptors:
+            self.liststore_manufacturers.append([item])
 
     def drawTreeView(self):
         renderer = Gtk.CellRendererText()
@@ -38,12 +45,24 @@ class VMTreeWidget(Gtk.Grid):
         renderer_combo.set_property("model", self.liststore_manufacturers)
         renderer_combo.set_property("text-column", 0)
         renderer_combo.set_property("has-entry", False)
+        renderer_combo.connect("edited", self.on_combo_changed)
         
         vmColumn = Gtk.TreeViewColumn("Virtual Machine", renderer, text=0)
-        adaptorColumn = Gtk.TreeViewColumn("Adaptor", renderer_combo, text=0)
+        adaptorColumn = Gtk.TreeViewColumn("Adaptor", renderer_combo, text=1)
         self.treeView.append_column(vmColumn)
         self.treeView.append_column(adaptorColumn)
-
+        
+    def on_combo_changed(self, widget, path, text):
+        logging.debug("on_combo_changed(): widget: " + str(widget) + " path: " + str(path) + " text: " + str(text))
+        chosenAdaptor = self.vmList[self.treeStore[path][0]]["adaptorInfo"][str(text)]
+        if "\"none\"" in chosenAdaptor or "adaptor disabled" in chosenAdaptor:
+            inputErrorDialog = Gtk.MessageDialog(self.parent, 0, Gtk.MessageType.ERROR,
+                Gtk.ButtonsType.OK, "The chosen adaptor is not currently enabled.\r\nChoose another adaptor.")
+            inputErrorDialog.run()
+            inputErrorDialog.destroy()
+            return
+        self.treeStore[path][1] = text + " - " + chosenAdaptor
+        
     def setLayout(self):
         self.scrollableTreeList.set_min_content_width(100)
         self.scrollableTreeList.set_min_content_height(100)
